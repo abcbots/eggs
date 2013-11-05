69ea0bc6e8a0be9140f6c320cdac9ff2f0601431
Dave Makena <davemakena@gmail.com>
2013-11-05 14:09:22 -0800

    fix NoMethodError...spec/eggs_spec.rb:7...first...for...8f...String

--- lib/eggs.rb
+++ lib/eggs.rb
@@ -1,40 +1,61 @@
+# == Module is reachable via module name: Eggs....
 module Eggs
 
-#== Eggs: Scramble
-# Eggs.scramble("dog", "dj4ki6bs9edh4jdu8mgbg3fi7s63eg") #=> ""
-# Eggs.scramble("cat", "dj4ki6bs9edh4jdu8mgbg3fi7s63eg") #=> ""
-# Eggs.scramble("bird", "dj4ki6bs9edh4jdu8mgbg3fi7s63eg") #=> ""
-  def self.scramble(text="", key=nil)
-    if text.present?
-      #url_encode aes(:encrypt, key, text)
-      URI.encode(aes(:encrypt, key, text.to_s))
-    else
-      ""
-    end
+# == Pry for debugging
+  require 'pry'
+
+# == Eggs: scramble (text, key)
+  # require 'eggs'
+  # key = "8sFpMOjupOgV9STuwPqOXBBqUDEcEy"
+  # text = scrambled = "dog"
+  # scrambled == text #=> true
+  # scrambled = Eggs.scramble(text, key) = "%ED%B6%E7%88%EC%A6g%81%17%A8%92]J%8A[%9C"
+  # scrambled == text #=> false
+  #
+  def self.scramble(text, key)
+    return URI.encode aes(:encrypt, key, text)
   end
 
-#== Eggs: Unscramble
-# Eggs.unscramble("...", "dj4ki6bs9edh4jdu8mgbg3fi7s63eg") #=> "dog"
-# Eggs.unscramble("...", "dj4ki6bs9edh4jdu8mgbg3fi7s63eg") #=> "cat"
-# Eggs.unscramble("...", "dj4ki6bs9edh4jdu8mgbg3fi7s63eg") #=> "bird"
-  def self.unscramble(text="", key=nil)
-    if ((key=key.to_s).present? and (text=text.to_s).present?)
-      aes(:decrypt, key, URI.decode(text))
-    else
-      ""
-    end
+# == Eggs: unscramble (text, key)
+  # require 'eggs'
+  # key == "TxgOH3A8bqwd8L0jbcWoAA9ErKyppY"
+  # text == "dog"
+  # scrambled = Eggs.scramble(text, key) = "%05d%E2%A7+%FE%1D%16m%3C%B8%9F%D1%7D%5El"
+  # scrambled == text #=> false
+  # unscrambled = Eggs.unscramble(text, key) = "dog"
+  # unscrambled == text #=> true
+  #
+  def self.unscramble(text, key)
+    aes(:decrypt, key, URI.decode(text))
   end
 
-#== Eggs: (Generate Random) Key
-# Eggs.key #=> "dj4ki6bs9edh4jdu8mgbg3fi7s63eg"
-# Eggs.key(10) #=> "i6bs9edh4j"
-# Eggs.key(3) #=> "jd3"
+# == Eggs: (generate a random) key
+  # require 'eggs'
+  # Eggs.key == "ENWi9n7n806DMU5USHE7Wbzy8iOZlc"
+  # Eggs.key.size == 30
+  # Eggs.key(10) == "6H3emLzzgw"
+  # Eggs.key(10).size == 10
+  # Eggs.key(3) == "aPY"
+  # Eggs.key(3).size == 3
+  # 
   def self.key(length=30)
     length=length.to_i
-    s = SecureRandom.base64(length).to_s
+    s = SecureRandom.base64(length).split("")[1..length].join("").to_s
     s = s.gsub("/", SecureRandom.hex(1).split("").first.to_s)
     s = s.gsub("+", SecureRandom.hex(1).split("").first.to_s)
     return s.gsub("==", SecureRandom.hex(1).split("").first.to_s)
+  end
+
+# == Eggs: aes (command, text, key)
+  # require 'eggs'
+  # key == "BBCYjirFf8tqs4D3nFhukrRrtK144E"
+  # text == "dog"
+  # Eggs.aes(:encrypt, text, key) == "�=c�O�m2`�V�p��Y�<m:"
+  # Src: http://stackoverflow.com/questions/8489486/encryption-and-decryption-algorithm-in-rails
+  # 
+  def self.aes(m,k,t)
+    (aes = OpenSSL::Cipher::Cipher.new('aes-256-cbc').send(m)).key = Digest::SHA256.digest(k)
+    return aes.update(t) << aes.final
   end
 
 end
--- spec/eggs_spec.rb
+++ spec/eggs_spec.rb
@@ -3,7 +3,54 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'eggs'
 
 describe "Eggs" do
-  it "generates a key" do
-    puts Eggs.key
+
+  it "generates keys" do
+    puts 
+    puts "# == Eggs: (generate a random) key"
+    puts "require 'eggs'"
+    puts "Eggs.key == \"#{Eggs.key}\""
+    puts "Eggs.key.size == #{Eggs.key.size}"
+    puts "Eggs.key(10) == \"#{Eggs.key(10)}\""
+    puts "Eggs.key(10).size == #{Eggs.key(10).size}"
+    puts "Eggs.key(3) == \"#{Eggs.key(3)}\""
+    puts "Eggs.key(3).size == #{Eggs.key(3).size}"
   end
+
+  it "scrambles" do
+    puts
+    puts "# == Eggs: scramble (text, key)"
+    puts "require 'eggs'"
+    puts "key = \"#{key = Eggs.key}\""
+    puts "text = scrambled = \"#{text = scrambled = "dog"}\""
+    puts "scrambled == text #=> #{(scrambled == text).to_s}"
+    (scrambled==text).should eq(true)
+    puts "scrambled = Eggs.scramble(text, key) = \"#{scrambled = Eggs.scramble(text, key)}\""
+    puts "scrambled == text #=> #{(scrambled == text).to_s}"
+    (scrambled==text).should eq(false)
+  end
+
+  it "unscrambles" do
+    puts
+    puts "# == Eggs: unscramble (text, key)"
+    puts "require 'eggs'"
+    puts "key == \"#{key = Eggs.key}\""
+    puts "text == \"#{text = "dog"}\""
+    puts "scrambled = Eggs.scramble(text, key) = \"#{scrambled = Eggs.scramble(text, key)}\""
+    puts "scrambled == text #=> #{(scrambled == text).to_s}"
+    (scrambled==text).should eq(false)
+    puts "unscrambled = Eggs.unscramble(text, key) = \"#{unscrambled = Eggs.unscramble(scrambled, key)}\""
+    puts "unscrambled == text #=> #{(unscrambled == text).to_s}"
+    (unscrambled==text).should eq(true)
+  end
+
+  it "uses OpenSSL libraries to encrypt text with a key" do
+    puts
+    puts "# == Eggs: aes (command, text, key)"
+    puts "require 'eggs'"
+    puts "key == \"#{key = Eggs.key}\""
+    puts "text == \"#{text = "dog"}\""
+    puts "Eggs.aes(:encrypt, text, key) == \"#{scrambled = Eggs.aes(:encrypt, text, key)}\""
+    puts "Src: http://stackoverflow.com/questions/8489486/encryption-and-decryption-algorithm-in-rails"
+  end
+
 end
